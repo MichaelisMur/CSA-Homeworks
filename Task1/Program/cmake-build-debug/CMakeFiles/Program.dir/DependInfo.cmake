e05a48e409c8794afc1769164669ff959d834c47
Geen Genade <michaelis.mur@gmail.com>
2021-10-03 23:09:29 +0300

    task1

---
+++ Task1/Program/cmake-build-debug/CMakeFiles/Program.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/circle.cpp" "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/cmake-build-debug/CMakeFiles/Program.dir/circle.cpp.obj"
+  "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/colour.cpp" "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/cmake-build-debug/CMakeFiles/Program.dir/colour.cpp.obj"
+  "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/container.cpp" "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/cmake-build-debug/CMakeFiles/Program.dir/container.cpp.obj"
+  "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/main.cpp" "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/cmake-build-debug/CMakeFiles/Program.dir/main.cpp.obj"
+  "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/rectangle.cpp" "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/cmake-build-debug/CMakeFiles/Program.dir/rectangle.cpp.obj"
+  "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/shape.cpp" "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/cmake-build-debug/CMakeFiles/Program.dir/shape.cpp.obj"
+  "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/triangle.cpp" "C:/Users/User/Desktop/CSA-Homeworks/Task1/Program/cmake-build-debug/CMakeFiles/Program.dir/triangle.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
